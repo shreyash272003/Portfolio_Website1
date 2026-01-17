47da625ea96024996524e6dded5db9d80f1fcba9
shreyash272003 <mr.shreyashjadhav@gmail.com>
2026-01-17 08:20:57 -0800

    third commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+function welcome_msg()
+{
+	alert("Welcome User On Portfolio Website...");
+}
+
+function message() 
+{
+	let name = document.getElementById("user_name1").value;
+	let msg = alert(name+" "+" , thanks for your precious contact.");
+}
+
